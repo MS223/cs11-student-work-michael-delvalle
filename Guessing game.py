d903908a6897793b3fe747271bab8ef50a0e2d92
michael-delvalle <michael.delvalle@ms223.org>
2016-04-05 08:50:56 -0400

    Update Guessing game.py

--- Guessing game.py
+++ Guessing game.py
@@ -2,13 +2,16 @@ range= input('pick a range')
 guess= input ('Pick a number within that range')
 
 import random
-random_numbrer = (random.randint(1,range))
-if guess < random
-    print "Guess a little higher"
-elif guess > random
-    print "Guess a little lower"
-else
-    print "You guessed right"
+random_number = (random.randint(1,range))
+"""Add a while loop"""
+while guess != random_number:
+    if guess < random_number:
+        print "Guess a little higher"
+        guess= input ('Guess again')
+    elif guess > random_number:
+        print "Guess a little lower"
+        guess= input ('Guess again')
+    # elif guess == random_number:
+print "You guessed right"
 
 """Get here on time next time. Don't take the bus even if its necessary! TAKE A CAB """
-# SK agrees.
