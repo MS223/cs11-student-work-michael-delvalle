a05e28f926e1c8773279041fdfae4e53154b4815
michael-delvalle <michael.delvalle@ms223.org>
2016-04-05 09:26:29 -0400

    Update Guessing game.py

--- Guessing game.py
+++ Guessing game.py
@@ -8,15 +8,18 @@ random_number = (random.randint(1,range))
 while guess != random_number:
     if guess < random_number:
         score = score + 1
+        # this code tells the player to guess higher when they have guessed to low. It also adds a point to the amount of times they have guessed.
         print "Guess a little higher"
         guess= input ('Guess again')
     elif guess > random_number:
         score = score + 1
         print "Guess a little lower"
+        # this code tells the player to guess lower when they have guessed to high. It also adds a point to the amount of times they have guessed.
         guess= input ('Guess again')
     # elif guess == random_number:
 print "You guessed right"
 print "The amount of times you guessed is " + str(score)
+#this code lets them know they have guessed correctly and gives them the total amount of times that they've guessed.
 
 # I did it :D
 """Get here on time next time. Don't take the bus even if its necessary! TAKE A CAB """
