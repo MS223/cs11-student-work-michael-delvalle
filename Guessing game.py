fd40a94801348fc12c8dbb1dda042d941606ac31
michael-delvalle <michael.delvalle@ms223.org>
2016-04-05 09:20:15 -0400

    Update Guessing game.py

--- Guessing game.py
+++ Guessing game.py
@@ -1,17 +1,22 @@
 range= input('pick a range')
 guess= input ('Pick a number within that range')
+score = 0
 
 import random
 random_number = (random.randint(1,range))
 """Add a while loop"""
 while guess != random_number:
     if guess < random_number:
+        score = score + 1
         print "Guess a little higher"
         guess= input ('Guess again')
     elif guess > random_number:
+        score = score + 1
         print "Guess a little lower"
         guess= input ('Guess again')
     # elif guess == random_number:
 print "You guessed right"
+print "The amount of times you guessed is " + str(score)
 
+# I did it :D
 """Get here on time next time. Don't take the bus even if its necessary! TAKE A CAB """
