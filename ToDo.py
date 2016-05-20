fba956b8cb75c791e5920eaad04758ebd19a60f5
michael-delvalle <michael.delvalle@ms223.org>
2016-05-20 08:43:56 -0400

    Create ToDo.py

---
+++ ToDo.py
@@ -0,0 +1,16 @@
+to_do = raw_input("What do you need to do?")
+day = raw_input("What day?").upper()
+days_of_the_week = {
+    "monday" :[],
+    "tuesday" :[],
+    "wednesday" :[],
+    "thursday" :[],
+    "friday" :[],
+    "saturday" :[],
+    "sunday" :[],
+}
+def add():
+    days_of_the_week["day"] = to_do .append()
+print days_of_the_week
+
+#I need to have the list print out with what I need to do that specific day
